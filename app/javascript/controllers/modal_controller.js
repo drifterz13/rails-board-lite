ea5b814cb92bcf1330f337531c422d070a3bdfe6
drifterz13 <mixmyxxe@gmail.com>
2022-11-27 11:53:19 +0700

    add close button for modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -8,5 +8,9 @@ export default class extends Controller {
 
   close() {
     this.element.hidden = true
+    const modalContent = this.element.querySelector('#modal_content')
+    while (modalContent.firstChild) {
+      modalContent.removeChild(modalContent.firstChild);
+    }
   }
 }
