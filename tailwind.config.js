6113f92075647b225f3918de0c3a07a981fe132f
drifterz13 <mixmyxxe@gmail.com>
2022-12-05 21:04:10 +0700

    add tailwind form plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require('@tailwindcss/forms'),],
   daisyui: {
     themes: ["corporate"],
   },
