b78a74e50e44965c5b353eb8ab40c82b0209276d
drifterz13 <mixmyxxe@gmail.com>
2022-12-05 21:04:35 +0700

    setup user avatar and active storage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  include LetterAvatar::AvatarHelper
+
   def render_turbo_stream_flash_messages
     turbo_stream.prepend "flash", partial: "layouts/flash"
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 #  updated_at             :datetime         not null
 #
 class User < ApplicationRecord
+  has_one_attached :avatar
   has_many :task_users, dependent: :destroy
   has_many :tasks, through: :task_users
 
